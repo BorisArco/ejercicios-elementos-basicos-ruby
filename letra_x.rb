76997a77928b9745ed9c3fb0e996c62604b05212
boris.arco <boris.arco@gmail.com>
2023-05-12 20:08:03 -0400

    add functions

--- letra_x.rb
+++ letra_x.rb
@@ -1,61 +1,70 @@
 n = ARGV[0].to_i
 
-if n < 3 || n.even?
-  puts "El valor de entrada debe ser un número impar mayor o igual a 3"
-  exit
-end
+#Implementar después
+#if n < 3 || n.even?
+#  puts "El valor de entrada debe ser un número impar mayor o igual a 3"
+#  exit
+#end
 
-#primera parte
-(n/2).times do |i|
+#Parte superior
+def parte_superior(n)
+  (n/2).times do |i|
 #lado izquierdo
-  i.times do
+    i.times do
+      print " "
+    end
+    print "*"
+    ((n/2)-1-i).times do
+      print " "
+    end
     print " "
+  #lado derecho
+    ((n/2)-1-i).times do
+      print " "
+    end
+    print "*"
+    (i-2).times do
+      print " "
+    end
+    print "\n"
   end
-  print "*"
-  ((n/2)-1-i).times do
-    print " "
-  end
-  print " "
-#lado derecho
-  ((n/2)-1-i).times do
-    print " "
-  end
-  print "*"
-  
-  (i-2).times do
-    print " "
-  end
-  print "\n"
 end
-
 
 #mitad
-if n%2 == 1
-  (n/2).times do
-    print " "
+def mitad(n)
+  if n%2 == 1
+    (n/2).times do
+      print " "
+    end
+    print "*"
+    print "\n"
   end
-  print "*"
-  print "\n"
 end
 
-
-#segunda parte
-(n/2).times do |j|
-  ((n/2)-1-j).times do
-    print " "
+#Parte inferior
+def parte_inferior(n)
+  (n/2).times do |j|
+    ((n/2)-1-j).times do
+      print " "
+    end
+    print "*"
+    j.times do
+      print " "
+    end
+    (1+j).times do
+      print " "
+    end
+    print "*"
+    (n-3-j).times do
+      print " "
+    end
+    print "\n"
   end
-  print "*"
-  j.times do
-    print " "
-  end
-
-  (1+j).times do
-    print " "
-  end
-  print "*"
-  (n-3-j).times do
-    print " "
-  end
-  
-  print "\n"
 end
+
+#Dibujar letra x
+
+parte_superior(n)
+mitad(n)
+parte_inferior(n)
+
