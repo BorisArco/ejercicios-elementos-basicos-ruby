6359e45dfa8e54774c97c227bffbc9911f847fd2
boris.arco <boris.arco@gmail.com>
2023-05-13 08:18:32 -0400

    add function and validation

--- patron1.rb
+++ patron1.rb
@@ -1,10 +1,21 @@
-n = gets.chomp.to_i
+# frozen_string_literal: true
 
-n.times do |i|
-  if i.even? == true
-    print "*"
-  else
-    print "." 
+numero = ARGV[0].chomp.to_i
+
+# Validación
+if numero <= 0
+  (puts 'El valor de entrada debe ser un número mayor 0'
+   exit)
+end
+
+def patron_punto_y_asterisco(numero)
+  numero.times do |i|
+    if i.even? == true
+      print '*'
+    else
+      print '.'
+    end
   end
 end
 
+patron_punto_y_asterisco(numero)
